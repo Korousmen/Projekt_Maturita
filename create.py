bd27fe8eb55b4d67e47534ef20c20f7a2bc606b7
Korousmen <119138870+Korousmen@users.noreply.github.com>
2025-03-18 19:44:17 +0100

    Update create.py
    Zaktualizování první tabulky a přidání druhé

--- create.py
+++ create.py
@@ -1,25 +1,47 @@
-import psycopg2
+import mysql.connector
 
 def create():
-    conn = psycopg2.connect(
-        dbname='projekt',
-        user='postgres',
-        password='heslo',
-        host='localhost',
-        port='5432'
-        )
+    conn = mysql.connector.connect(
+        host='dbs.spskladno.cz',
+        port=3306,
+        user='student8',
+        password='spsnet',
+        database='vyuka8'
+    )
 
     cur = conn.cursor()
-    cur.execute('''CREATE TABLE uziv(
-            ID serial,
+    cur.execute('''CREATE TABLE IF NOT EXISTS uziv(
+            ID INT AUTO_INCREMENT PRIMARY KEY,
             username TEXT,
             email TEXT,
             password TEXT,
-            pocet_her INT DEFAULT 0  ,
-            pocet_vyher INT  DEFAULT 0 
-                    )''')
+            user_type_id INT DEFAULT 2,
+            pocet_her INT DEFAULT 0,
+            pocet_vyher INT DEFAULT 0
+        );'''
+    )
+    
+    conn.commit()
+    conn.close()
+
+def create2():
+    conn = mysql.connector.connect(
+        host='dbs.spskladno.cz',
+        port=3306,
+        user='student8',
+        password='spsnet',
+        database='vyuka8'
+    )
+
+    cur = conn.cursor()
+    cur.execute('''CREATE TABLE IF NOT EXISTS uziv_type(
+            ID INT AUTO_INCREMENT PRIMARY KEY,
+            user_type TEXT
+        );'''
+    )
     
     conn.commit()
     conn.close()
 
 create()
+create2()
